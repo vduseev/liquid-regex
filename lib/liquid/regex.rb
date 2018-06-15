9de7ea6b47774f92da79063bc6dc832c130c17ca
vduseev <vagiz.d@gmail.com>
2018-06-15 17:34:20 +0700

    Initial commit

---
+++ lib/liquid/regex.rb
@@ -0,0 +1,7 @@
+require "liquid/regex/version"
+
+module Liquid
+  module Regex
+    # Your code goes here...
+  end
+end
---
+++ lib/liquid/regex/version.rb
@@ -0,0 +1,5 @@
+module Liquid
+  module Regex
+    VERSION = "0.1.0"
+  end
+end
