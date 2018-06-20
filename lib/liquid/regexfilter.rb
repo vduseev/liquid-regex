58d291c29bba28232ef472600cbf4c949c6d7dbd
vduseev <vagiz.d@gmail.com>
2018-06-20 18:52:11 +0700

    First implementation

--- lib/liquid/regexfilter.rb
+++ lib/liquid/regexfilter.rb
@@ -2,6 +2,21 @@ require "liquid/regexfilter/version"
 
 module Liquid
   module RegexFilter
-    # Your code goes here...
+    def regex(input, regexp, replacement = nil)
+      # Return input unchanged unless it's a string
+      return input unless input.is_a?(String)
+
+      # Construct a regular expression
+      expr = /#{regexp}/
+
+      if replacement
+        # Return input with replaced substring
+        input.gsub(expr, replacement)
+      else
+        m = expr.match(input)
+        # Return matched part if success, otherwise - unchanced input
+        m ? m.to_s : input
+      end
+    end
   end
 end
